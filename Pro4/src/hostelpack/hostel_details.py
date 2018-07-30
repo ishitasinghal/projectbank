7fd52f330ddbdd966cd11d623b2a66d8164505e4
Ishita Singhal <ishitasinghal08@gmail.com>
2018-07-30 23:21:20 +0530

    Project packed

---
+++ Pro4/src/hostelpack/hostel_details.py
@@ -0,0 +1,214 @@
+#vatika girls hostel
+def vatika_girls():
+    from hostelpack import hostel_details2
+    name="Vatika Girls Hostel"
+    about='''Vatika Girls Hostel is the best girls hostel in Dehradun near Bidholi.
+Vatika Girls Hostel give full surety  for safety and security for girls in our hostel,
+best hygienic food , clean rooms, bus transport and many more facilities.
+It has proven to be one of the best hostels in Dehradun for the last 10 years.'''
+    rooms="Triple sharing"
+    transport="Bus and hostel vans available"
+    price="Rs1,25,000 per annum"
+    facilities='''
+    One wooden bed with box & curved comfortable back.
+    One pillow with cover.
+    One Dunlop mattress with a bed sheet.
+    One chair study table.
+    One steel almirah.
+    Green & clean surroundings.
+    Rooms & Washrooms are spacious & well ventilated.
+    Window with curtains.
+    Attached washroom.
+    Washrooms are furnished with wall to wall tiles & Showers.
+    Bucket, mug, mirror, stool & hanger in every bathroom.
+    24 hours water supply with the submersible pump.
+    Aqua guard water cooler in each and every floor.
+    Hot water in the winter season.
+    Healthy & hygienic meals.
+    Daily newspapers (Hindi / English).
+    V. with cable connection.
+    Recreation room with Table Tenis table
+    Badminton court.
+    Well, maintain Gym& occupied with proper machinery.
+    Providing & maintaining first aid kit.
+    Doctor facility on call.
+    Laundry facility.
+    Power backup.
+    24 hours High-Speed wi-fi (30-GB Downloading /month/student)
+    Spacious dining hall.
+    24 hours High security.
+    Conveyance facility.
+    Spacious parking facility for two wheelers only.
+    24 hours resident warden.
+'''
+    book_now='''Contact For Girls Hostel  BOOKING:
+Vatika Girls Hostel
+Address: 111, Bidholi, Dehradun (UK) 248007
+Phone: 9927726767, 9117165633
+Email: vatikagirls@gmail.com'''
+    print("------>>>>")
+    hostel_details2.obj1.select(name,about,rooms,transport,price,facilities,book_now)
+
+#vatika boys hostel
+def vatika_boys():
+    from hostelpack import hostel_details2
+    name="Vatika Boys Hostel"
+    about='''Vatika Boys Hostel is the best Boys hostel in Dehradun near Bidholi.
+Vatika Boys Hostel give full surety  for safety for boys in our hostel,
+best hygienic food , clean rooms, bus transport and many more facilities.
+It has proven to be one of the best hostels in Dehradun for the last  years.'''
+    rooms="Triple sharing"
+    transport="Bus and hostel vans available"
+    price="Rs1,25,000 per annum"
+    facilities='''
+    One wooden bed with box & curved comfortable back.
+    One pillow with cover.
+    One Dunlop mattress with a bed sheet.
+    One chair study table.
+    One steel almirah.
+    Green & clean surroundings.
+    Rooms & Washrooms are spacious & well ventilated.
+    Window with curtains.
+    Attached washroom.
+    Washrooms are furnished with wall to wall tiles & Showers.
+    Bucket, mug, mirror, stool & hanger in every bathroom.
+    24 hours water supply with the submersible pump.
+    Aqua guard water cooler in each and every floor.
+    Hot water in the winter season.
+    Healthy & hygienic meals.
+    Daily newspapers (Hindi / English).
+    V. with cable connection.
+    Recreation room with Table Tenis table
+    Badminton court.
+    Well, maintain Gym& occupied with proper machinery.
+    Providing & maintaining first aid kit.
+    Doctor facility on call.
+    Laundry facility.
+    Power backup.
+    24 hours High-Speed wi-fi (30-GB Downloading /month/student)
+    Spacious dining hall.
+    24 hours High security.
+    Conveyance facility.
+    Spacious parking facility for two wheelers only.
+    24 hours resident warden.
+'''
+    book_now='''Contact For Boys Hostel  BOOKING:
+Vatika Boys Hostel
+Address: 115, Bidholi, Dehradun (UK) 248007
+Phone: 9927726767, 9117165633
+Email: vatikaboys@gmail.com'''
+    hostel_details2.obj2.select(name,about,rooms,transport,price,facilities,book_now)
+
+
+
+#aman residency(boys)
+def aman_residency():
+    from hostelpack import hostel_details2
+    name="Aman Residency"
+    about='''Aman Residency near Prem Nagar, Dehradun provides  
+best hygienic food , clean rooms, washrooms, transportion and many more facilities.'''
+    rooms="Double sharing"
+    transport="vans available"
+    price="Rs1,20,000 per annum"
+    facilities='''
+>>    One wooden bed with box.
+>>    One Dunlop mattress.
+>>    One chair study table.
+>>    One steel almirah.
+>>    Rooms & Washrooms are spacious & well ventilated.
+>>    Window with curtains.
+>>    Washrooms are furnished with wall to wall tiles & Showers.
+>>    24 hours water supply with the submersible pump.
+>>    Aqua guard water cooler in each and every floor.
+>>    Hot water 24*7.
+>>    Healthy & hygienic meals.
+>>    T.V. with cable connection.
+>>    Recreation room with Table Tenis table, badminton court.
+>>    Well, maintain Gym& occupied with proper machinery.
+>>    Providing & maintaining first aid kit.
+>>    Laundry facility 3 days a week.
+>>    24 hours High-Speed wi-fi (30-GB Downloading /month/student)
+>>    Spacious dining hall.
+>>    24 hours High security.
+>>    Conveyance facility.
+>>    Spacious parking facility for two wheelers only.
+'''
+    book_now='''Contact For Boys Hostel  BOOKING:
+Aman Residency
+Address: 101, Prem Nagar, Dehradun (UK) 248007
+Phone: 9927726767, 9117165633
+Email: amanresidency@gmail.com'''
+    hostel_details2.obj3.select(name,about,rooms,transport,price,facilities,book_now)
+
+
+
+
+#sarthak boys hostel
+def sarthak():
+    from hostelpack import hostel_details2
+    name="Sarthak Boys Hostel"
+    about='''Sarthak Boys Hostel is available to Students for the academic year
+2018-2019 at an affordable package. Rent includes food and lodging with a complete
+home like environment. Distance from UPES College is Just 500 meters that saves your
+Time, Money and Energy on travel.The Hostel is ideal for UPES College Students.'''
+    rooms="Double sharing"
+    transport="vans available on Saturdays and Sundays"
+    price="Rs1,00,000 per annum"
+    facilities='''
+->      Spacious and airy rooms on Twin Sharing Basis
+->      Luxurious rooms with Attached Bathroom.
+->      Fully furnished rooms with Bed, Mattress, Pillow, Steel Almira, Study Table, Chairs
+->      Wi-Fi Internet 50 GB/mth.
+->      Laundry Facilitiy Available
+->      Spacious Dining Hall with Television
+->      Delicious and nutritious food, cooked by professional cooks
+->      R.O. Drinking water
+->      Water Dispenser for Hot & Cold Drinking water
+->      Power back-up Inverter & Generator
+->      A Clean, Healthy & Stress-Free environment ideal for a scholar'''
+    book_now='''Contact For Boys Hostel  BOOKING:
+Sarthak Boys Hostel
+Address: 121, Bidholi, Dehradun (UK) 248007
+Phone: 9117725867, 8123123563
+Email: sarthak@gmail.com'''
+    hostel_details2.obj4.select(name,about,rooms,transport,price,facilities,book_now)
+
+
+
+
+
+
+#pride girls hostel
+def pride():
+    from hostelpack import hostel_details2
+    name="Pride Girls Hostel"
+    about='''At Pride Girls Hostel, the idea is to make one as comfortable
+as possible in this parallel culture. There are common rooms and courts for various
+sports, where the students can relax and cool down after a hectic day of work.
+Sweet Home Girls Hostel Located just near ICFAI University Dehradun.
+Sweet Homes Girls Hostel is a place which instils a sense of wider community,
+discipline and hard work and awakens a sense of belongingness.'''
+    rooms="Double sharing"
+    transport="vans and bus available"
+    price="Rs95,000 per annum"
+    facilities='''
+*       Wifi Internet Connectivity in the Hostel.
+*       24 Hours power backup
+*       Round the clock security with CCTV Cameras.
+*       24 * 7 Medical facility
+*       24 Hrs water supply
+*       Purify Drinking water
+*       Departmental store
+*       Mess with Hygiene food
+*       Sports - Indoor Games, Badminton, etc.
+*       Laundry facility.
+*       Free Pick and drop from Colleges.'''
+    book_now='''Contact For Girls Hostel  BOOKING:
+Pride Girls Hostel
+Address: 121, Prem Nagar, Dehradun (UK) 248007
+Phone: 9123456787, 8156565663
+Email: pride@gmail.com'''
+    hostel_details2.obj5.select(name,about,rooms,transport,price,facilities,book_now)
+
+
+  
